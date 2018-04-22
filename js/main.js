6857e0ef0e0e1a2ff23f7cb4fc00107e03aa3e3c
Jesse Rogers <jesse.rogers@uconn.edu>
2018-04-22 19:40:05 -0400

    Cozy Styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+// Click the X button to remove an entry
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
 
+//Click the list item to cross it out
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("complete");
+});
+
+//Add new entries to the list by pressing the enter key
+$("input").keypress(function(enter){
+	if(enter.which === 13){
+		var value = $(this).val();
+		$("ul").append("<li><span><i class='fa fa-trash-alt'></i></span> " + value + "</li>");
+		$(this).val("");
+	}
+});
+
+$(".fa-plus").on("click", function(){
+	$("input").fadeToggle(400);
+});
